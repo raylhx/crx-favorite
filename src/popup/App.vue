aae5ff3cfc29b474a284a337f972e409e6fc43be
ray <raylaihx@163.com>
2020-08-03 17:56:06 +0800

    feat: edit popup

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <div>
-    <p>Hello world!</p>
+  <div class="popup-wrapper">
+    <div class="container">
+      <div class="items">
+        Article title
+        <input type="text" v-model="title"/>
+      </div>
+      <div class="items">
+        Recommend reason
+        <textarea v-model="reason"></textarea>
+      </div>
+      <div class="items">
+        Set category
+        <select>
+          <option>test</option>
+        </select>
+      </div>
+    </div>
+    <div class="submit-btn" @click="commit">Commit</div>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      title: '',
+      reason: '',
+      categor: '',
+      username: ''
+    }
+  },
+  methods: {
+    commit () {
+
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 p {
   font-size: 20px;
 }
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -4,6 +4,7 @@ import App from './App'
 global.browser = require('webextension-polyfill')
 Vue.prototype.$browser = global.browser
 
+console.log('hhhhh')
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
