8f12a072ababfbc9bcc3de9e862c30f07dd756f3
ray <raylaihx@163.com>
2020-08-05 15:24:23 +0800

    fix: 重复声明都dom去掉

--- src/recommend.js
+++ src/recommend.js
@@ -1,9 +1,8 @@
 console.log('------recommend load')
-const dom = document
 chrome.runtime.onMessage.addListener((request, render, sendResponse) => {
   if (request.message === 'GET_TOPIC_DATA') {
-    const title = dom.getElementsByTagName('title')[0].textContent
-    const descriptionElement = dom.querySelectorAll('meta[name=description]')[0]
+    const title = document.getElementsByTagName('title')[0].textContent
+    const descriptionElement = document.querySelectorAll('meta[name=description]')[0]
     const description = descriptionElement ? descriptionElement.getAttribute('content') : title
     const link = location.href
 
