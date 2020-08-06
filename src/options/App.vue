1bfdfb34e3c3199c8454c2466d1f4e23683f1546
ray <raylaihx@163.com>
2020-08-06 10:13:14 +0800

    feat: 添加option简单选项

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
+  <div class="app">
+    <div class="favourite-name">
+      <div>Nickname</div>
+      <div><input type="text" v-model="nickName"/></div>
+      <div class="save-btn" @click="save">Save</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      nickName: ''
+    }
+  },
+  methods: {
+    save () {
+      console.log('-----------', this.nickName)
+      chrome.storage.sync.set({ nickName: this.nickName }, () => {
+        console.log('nickname')
+      })
+    }
+  }
 }
 </script>
 
